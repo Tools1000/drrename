910c851e9b2adc8817eaca6b1d0294df51733a93
Alexander Kerner <alexkerner1000@gmail.com>
2022-07-02 19:13:53 +0200

    Prepare sh for releases

--- src/main/resources/build-dmg.sh
+++ src/main/resources/build-dmg.sh
@@ -1,1 +1,3 @@
-/usr/bin/jpackage --name DrRename --input target --main-jar drrename-0.5.2-SNAPSHOT.jar --resource-dir src/main/resources/icons --dest target --app-version 1.0.0 --copyright copyrightstring --description descriptionstring --vendor vendorstring --java-options '--enable-preview'
+#!/usr/bin/env bash
+echo "Main jar:" "$1".jar
+/usr/bin/jpackage --name DrRename --input target --main-jar "$1".jar --resource-dir src/main/resources/icons --dest target --app-version 1.0.0 --copyright copyrightstring --description descriptionstring --vendor vendorstring --java-options '--enable-preview'
