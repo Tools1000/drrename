b488f7bc2545a8cceb4055a64d18805a9cae9273
Alexander Kerner <alexkerner1000@gmail.com>
2022-07-07 14:00:03 +0200

    Refactorings

---
+++ src/main/java/com/kerner1000/drrename/mainview/MainViewConfig.java
@@ -0,0 +1,14 @@
+package com.kerner1000.drrename.mainview;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "app.mainview")
+public class MainViewConfig {
+
+    private boolean debug;
+
+   }
