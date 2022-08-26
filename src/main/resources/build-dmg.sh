99af934a098daba088fd43067ef58e3023ff916d
Alexander Kerner <alexkerner1000@gmail.com>
2022-08-26 12:13:42 +0200

    Continue

--- src/main/resources/build-dmg.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-echo "Main jar:" "$1".jar
-h="$2"
-#${h/%-SNAPSHOT
-/usr/bin/jpackage --type pkg --name DrRename --input target --main-jar "$1".jar --resource-dir src/main/resources/icons --dest target --app-version 1.0.0 --copyright copyrightstring --description descriptionstring --vendor vendorstring --java-options '--enable-preview' --mac-package-identifier com.drrename
