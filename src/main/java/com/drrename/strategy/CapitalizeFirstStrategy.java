de3f5569de1971723412fc45b4e15602d183957d
Alexander Kerner <alexkerner1000@gmail.com>
2022-08-27 14:10:47 +0200

    Add strategies

---
+++ src/main/java/com/drrename/strategy/CapitalizeFirstStrategy.java
@@ -0,0 +1,27 @@
+package com.drrename.strategy;
+
+import com.github.drrename.strategy.RenamingStrategyProto;
+import org.apache.commons.text.CaseUtils;
+import org.apache.commons.text.WordUtils;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+public class CapitalizeFirstStrategy extends RenamingStrategyProto {
+    @Override
+    public String getIdentifier() {
+        return "Capitalize First Letter";
+    }
+
+    @Override
+    public String getNameNew(Path file) throws IOException, InterruptedException {
+        if(Thread.currentThread().isInterrupted())
+            throw new InterruptedException("Cancelled");
+        return WordUtils.capitalize(file.getFileName().toString(), ' ', '_','-');
+    }
+
+    @Override
+    public boolean isReplacing() {
+        return false;
+    }
+}
