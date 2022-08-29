f8d85f3d6634db3755c4271f0bfddf919061b4be
Alexander Kerner <alexkerner1000@gmail.com>
2022-08-29 19:56:01 +0200

    Extract Task

---
+++ src/main/java/drrename/kodi/KodiTask.java
@@ -0,0 +1,107 @@
+package drrename.kodi;
+
+import javafx.concurrent.Task;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FilenameUtils;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+class KodiTask extends Task<KodiCheckResult> {
+    private Path directory;
+
+    public KodiTask(Path directory) {
+        this.directory = directory;
+    }
+
+    @Override
+    protected KodiCheckResult call() throws Exception {
+        KodiCheckResult result = new KodiCheckResult();
+        try (DirectoryStream<Path> ds = Files.newDirectoryStream(directory)) {
+            for (Path path : ds) {
+                if (Thread.interrupted()) {
+                    log.info("Cancelling");
+                    break;
+                }
+//                        log.debug("Path {}", path);
+                if (Files.isDirectory(path)) {
+                    String movieName = path.getFileName().toString();
+                    if (movieName.startsWith("@")) {
+                        log.info("Ignoring {}", path);
+                        continue;
+                    }
+//                            log.debug("Movie name: {}", movieName);
+                    var result2 = checkSubDirs(path);
+                    if (!result2.getSubdirs().getSubdirs().isEmpty()) {
+                        result.addResult(result2);
+                    }
+                    var result3 = checkForNfoFile(path);
+                    if (KodiCheckResultElementNfoFile.NfoFile.INVALID_NAME.equals(result3) || KodiCheckResultElementNfoFile.NfoFile.NO_FILE.equals(result3))
+                        result.addResult(new KodiCheckResultElementNfoFile(movieName, result3));
+                }
+            }
+        }
+        return result;
+    }
+
+    private KodiCheckResultElementSubDirs checkSubDirs(Path path) throws IOException {
+        String movieName = path.getFileName().toString();
+        List<Path> subdirs = checkSubDirs2(path);
+        if (subdirs.isEmpty()) {
+            return new KodiCheckResultElementSubDirs(movieName, new SubdirResult(path, subdirs));
+        }
+        return new KodiCheckResultElementSubDirs(movieName, new SubdirResult(path, subdirs));
+    }
+
+    private List<Path> checkSubDirs2(Path path) throws IOException {
+        List<Path> subdirs = new ArrayList<>();
+        String movieName = path.getFileName().toString();
+        try (DirectoryStream<Path> ds = Files.newDirectoryStream(path)) {
+            for (Path child : ds) {
+                if (Files.isDirectory(child)) {
+                    subdirs.add(child.getFileName());
+                    log.debug("{} has subdir: {}, empty: {}", movieName, child, isEmpty(child));
+                }
+            }
+        }
+        if (!subdirs.isEmpty())
+            log.warn("{} has subdirs: {}", movieName, subdirs);
+        return subdirs;
+    }
+
+    private KodiCheckResultElementNfoFile.NfoFile checkForNfoFile(Path path) throws IOException {
+        String movieName = path.getFileName().toString();
+        try (DirectoryStream<Path> ds = Files.newDirectoryStream(path)) {
+            for (Path child : ds) {
+                String extension = FilenameUtils.getExtension(child.getFileName().toString());
+                if (Files.isRegularFile(child) && "nfo".equalsIgnoreCase(extension)) {
+                    String childName = child.getFileName().toString();
+                    if ("movie.nfo".equalsIgnoreCase(childName)) {
+                        return KodiCheckResultElementNfoFile.NfoFile.DEFAULT_NAME;
+                    } else if (FilenameUtils.getBaseName(childName).equalsIgnoreCase(movieName)) {
+                        return KodiCheckResultElementNfoFile.NfoFile.MOVIE_NAME;
+                    } else {
+                        log.warn("Invalid nfo file name for movie {}: {}", movieName, childName);
+                        return KodiCheckResultElementNfoFile.NfoFile.INVALID_NAME;
+                    }
+                }
+            }
+        }
+        log.warn("no nfo file for {}", movieName);
+        return KodiCheckResultElementNfoFile.NfoFile.NO_FILE;
+    }
+
+    public static boolean isEmpty(Path path) throws IOException {
+        if (Files.isDirectory(path)) {
+            try (DirectoryStream<Path> directory = Files.newDirectoryStream(path)) {
+                return !directory.iterator().hasNext();
+            }
+        }
+        return false;
+    }
+}
