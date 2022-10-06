a611d6bc327dd41bab8a935c1715c9fa8cc50e5a
Alexander Kerner <alexkerner1000@gmail.com>
2022-10-06 17:03:41 +0200

    Add module config

---
+++ src/main/java/module-info.java
@@ -0,0 +1,25 @@
+open module drrename {
+    requires java.sql;
+    requires org.slf4j;
+    requires lombok;
+    requires spring.boot;
+    requires spring.context;
+    requires javafx.graphics;
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires net.rgielen.fxweaver.core;
+    requires java.annotation;
+    requires spring.beans;
+    requires spring.core;
+    requires org.apache.commons.lang3;
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.dataformat.xml;
+    requires org.apache.commons.io;
+    requires org.apache.tika.core;
+    requires net.rgielen.fxweaver.spring;
+    requires spring.boot.autoconfigure;
+    requires org.apache.commons.text;
+    requires metadata.extractor;
+    requires jodd.util;
+}
